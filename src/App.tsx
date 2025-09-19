9ca75fdf152bd1d22c40835e084bea1011201dbf
steeven-louk <steevenlouk.sl@gmail.com>
2025-09-19 15:27:25 +0200

    initialisation des routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import GuestList from './pages/guestList';
+import Tables from './pages/tables';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Router>
+      <nav className="p-4 bg-gray-200 flex gap-4">
+        <Link to="/">Invités</Link>
+        <Link to="/tables">Tables</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<GuestList />} />
+        <Route path="/tables" element={<Tables />} />
+      </Routes>
+    </Router>
     </>
   )
 }
