016491734b460799019ea384a73ea16ba04b9b06
steeven-louk <steevenlouk.sl@gmail.com>
2025-09-27 10:20:20 +0200

    .

--- src/pages/tables.tsx
+++ src/pages/tables.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useGuestStore } from "../store/guestStore";
 import type { Guest } from "../types/ghestType";
 import { RxCross1 } from "react-icons/rx";
